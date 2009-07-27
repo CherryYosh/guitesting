a2798f86d902ecdac792d96328f97ef544b83624
brandon <j.brandon.s@gmail.com>
2009-07-27 20:10:17 +0000

    Forgot to add edge class

---
+++ src/gui/edge.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   edge.cpp
+ * Author: brandon
+ * 
+ * Created on July 12, 2009, 7:03 PM
+ */
+
+#include "edge.h"
+#include "window.h"
+
+Edge::Edge() { }
+
+Edge::Edge(const Edge& orig) { }
+
+Edge::~Edge() { }
+
+Control* Edge::clone() {
+    return new Edge(*this);
+}
+
+void Edge::OnMousePress(unsigned short button, int x, int y) {
+    LockMouse();
+
+    oldx = x;
+    oldy = y;
+}
+
+bool Edge::OnMouseMotion(float lx, float ly, unsigned short button) {
+    if (button == 1) { //TODO: Unhard-code
+	root->Resize(lx - oldx, ly - oldy);
+
+	oldx = lx;
+	oldy = ly;
+
+	return true;
+    }
+
+    return false;
+}
---
+++ src/gui/edge.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   edge.h
+ * Author: brandon
+ *
+ * Created on July 12, 2009, 7:03 PM
+ */
+
+#ifndef _EDGE_H
+#define	_EDGE_H
+
+#include "control.h"
+
+class Edge : public Control{
+public:
+    Edge();
+    Edge(const Edge& orig);
+    virtual ~Edge();
+
+    Control* clone();
+
+    void OnMousePress(unsigned short, int, int);
+    bool OnMouseMotion(float, float, unsigned short);
+
+private:
+    float oldx, oldy;
+};
+
+#endif	/* _EDGE_H */
+
